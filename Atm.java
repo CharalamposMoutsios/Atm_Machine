9c6472d4e37645ae753cae4d65af369878909cf3
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-04-29 23:22:53 +0200

    atm_machine

---
+++ Atm.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class Atm {
+
+	public static void main(String[] args) throws IOException {
+		Option_Menu optionMenu = new Option_Menu();
+		introduction();
+		optionMenu.mainMenu();
+	}
+
+	public static void introduction() {
+		System.out.println("Welcome to the ATM Project!");
+	}
+}
